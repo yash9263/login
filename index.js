98f8b488f097b6bf0f794a4ebbd2bc37bd42e6b3
Workspace <yashwant@gmail.com>
2020-08-10 23:14:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost:27017/userDB`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
