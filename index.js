342cd52895753b6f41e08124f4ff3a1018eab368
Workspace <yashwant@gmail.com>
2020-08-11 20:24:00 +0530

    added routes and models

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// /index.js
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+// IMPORT MODELS
+require("./models/Product");
 
 const app = express();
 mongoose.Promise = global.Promise;
@@ -10,6 +14,18 @@ mongoose.connect(
 );
 
 app.use(bodyParser.json());
+
+//IMPORT ROUTES
+require("./routes/productRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ models/Product.js
@@ -0,0 +1,11 @@
+// /models/Product.js
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: String,
+  description: String,
+});
+
+mongoose.model("products", productSchema);
